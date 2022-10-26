673d269024e94cd362bb22b9f23081b04c22602d
comblock <81411315+comblock@users.noreply.github.com>
2022-10-26 22:01:31 +0200

    Added concrete connection types and a packet trait

---
+++ net/src/packet.rs
@@ -0,0 +1,8 @@
+pub use miners::packet::Packet;
+
+pub trait PacketBuilderExt: Packet {
+    type Data;
+    fn new(data: Self::Data) -> Self;
+}
+
+// TODO: Create the PacketData types and implement PacketBuilderExt for 
