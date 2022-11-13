c754c1db2f1e206ab5077be38612f12184112a33
comblock <81411315+comblock@users.noreply.github.com>
2022-11-13 23:06:33 +0100

    added chunk data structures

--- level/src/chunk.rs
+++ level/src/chunk.rs
@@ -1,1 +1,11 @@
+use crate::{palette::{StatePaletteContainer, BiomePaletteContainer}, bitpack::PackedBits};
+pub struct ChunkColumn {
+    pub motion_blocking: PackedBits<256>, // len = 256 bits = 9
+    pub sections: Vec<Option<ChunkSection>>,
+}
 
+pub struct ChunkSection {
+    pub block_count: u16,
+    pub states: StatePaletteContainer<4096>, // 16*16*16 = 4096
+    pub biomes: BiomePaletteContainer<64>, // 4*4*4 = 64
+}
